58fca682f79712659eec0a5591251e9bda21aa89
MaxieTheHatter <maxnelson90@gmail.com>
2017-02-21 22:59:35 +0100

    added some validators that use JavaScript

---
+++ assets/script/validator.js
@@ -0,0 +1,64 @@
+function validateID() {
+	var id = document.getElementById("idNumber").value;
+	if(id.length < 1) {
+		document.getElementById("idNumberResult").innerHTML = "&#10068";
+	}else if(idChk(id)) {
+		document.getElementById("idNumberResult").innerHTML = "&#10003";
+	}else{
+		document.getElementById("idNumberResult").innerHTML = "&#10007";
+	}
+}
+
+function idChk(id){
+	if(id.length === 12) {
+		id = id.substring(2);
+	}
+	
+	var sum = 0;
+	var digits = id.length;
+	var parity = digits % 2;
+	
+	for(var i = 0; i < digits; i++) {
+		var num = parseInt(id.charAt(i))
+		if(i % 2 == parity) num *= 2;
+		if(num > 9) num -= 9;
+		sum += num;
+	}
+	return (sum % 10) == 0;
+}
+
+function validateLeapYear() {
+	var year = document.getElementById("leapYear").value;
+	if(year.length < 1) {
+		document.getElementById("leapYearResult").innerHTML = "&#10068";
+	}else if(leapChk(year)) {
+		document.getElementById("leapYearResult").innerHTML = "&#10003";
+	}else{
+		document.getElementById("leapYearResult").innerHTML = "&#10007";
+	}
+}
+
+function leapChk(year) {
+	if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function validateNumberSum() {
+	var numSum = document.getElementById("numberSum").value;
+	var sum = numChk(numSum);
+	document.getElementById("numberSumResult").innerHTML = sum;
+	
+}
+  
+function numChk(numSum) {
+	var str = numSum.toString();
+	var sum = 0;
+	
+	for(var i = 0; i < str.length; i++) {
+		sum += parseInt(str.charAt(i), 10);
+	}
+	return sum;
+}
