b3090d2734a245eae8e3e14b58a41968d89f76b4
MaxieTheHatter <maxnelson90@gmail.com>
2017-02-27 16:36:44 +0100

    started on examination assignment, working recepie

---
+++ assets/script/recepie.js
@@ -0,0 +1,58 @@
+var baseIngr = [];
+
+function setLocalCakeCount() {
+    "use strict";
+    // Code borrowed from w3C.
+    // Check browser support
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        if (window.localStorage.getItem("panCount")) {
+            var localCakeCount;
+            localCakeCount = document.getElementById("pancakes").value;
+            window.localStorage.setItem("panCount", localCakeCount);
+        } else {
+            window.localStorage.setItem("panCount", 1);
+        }
+    }
+}
+
+
+function updateRecipe() {
+    "use strict";
+    var pancakes = document.getElementById("pancakes").value;
+    if (pancakes < 1) {
+        pancakes = 1;
+    }
+    var asideElement = document.getElementsByTagName("aside")[0];
+    var ingrList = asideElement.getElementsByTagName("li");
+    for (var i = 0; i < ingrList.length; i++) {
+        var tempStr = ingrList[i].textContent;
+        var tempNumber = parseFloat(tempStr.match(/[0-9 | ^.]+/));
+        if (typeof (baseIngr[i]) === "undefined") {
+            baseIngr[i] = tempNumber;
+        }
+        tempNumber = (baseIngr[i] * pancakes);
+		tempNumber = Math.round(tempNumber * 100) / 100; //round to 2 decimals
+        tempStr = tempStr.replace(/[0-9 | ^.]+/, tempNumber + " "); //replace ingredients, add space at end
+        ingrList[i].textContent = tempStr; //update ingredient list
+    }
+    setLocalCakeCount(); //save selected number to localStorage
+}
+
+window.oninput = function () {
+    "use strict";
+    updateRecipe();
+};
+
+window.onload = function () {
+    "use strict";
+    // Code borrowed from w3C.
+    // Check browser support
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        if (window.localStorage.getItem("panCount")) {
+            document.getElementById("pancakes").value = parseInt(window.localStorage.getItem("panCount"));
+            updateRecipe();
+        }
+    }
+};
