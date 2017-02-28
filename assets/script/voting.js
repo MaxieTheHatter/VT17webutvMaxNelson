b38868ad865bb301aebd53b24e5d4069cbc38af2
MaxieTheHatter <maxnelson90@gmail.com>
2017-02-28 22:49:06 +0100

    updated styling, calling it done

--- assets/script/voting.js
+++ assets/script/voting.js
@@ -1,13 +1,29 @@
 //send rating to API
 function putVoteInDB (rating) {
     $.ajax({
-        method: "GET",
         url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=ff5ec25ea3f6b140&recipe=pannkakor&rating=" + rating,
+		type: "GET",
         success: function(data) {  
             updateVotingResult();
         }, 
     });
 }
+
+//get rating from API
+function updateVotingResult () {
+    $.ajax({
+        url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=ff5ec25ea3f6b140&recipe=pannkakor",
+		type: "GET",
+        success: function(data) {
+            var rating = data.rating.toFixed(1); //round rating to 1 decimal
+			var votes = data.votes;
+			$("#loading").html("");
+            $(".voteResult #rating").html("<b>"+rating+"</b>");
+            $(".voteResult #total").html("<b>"+votes+"</b>");
+        },
+    });
+}
+
 //add click and hover eventhandlers
 $(document).ready(function(){	
 	$(".voteIndicator span").click(function() {
@@ -16,17 +32,29 @@ $(document).ready(function(){
 			$(this).text("\u2605"); //set chosen star to black            
             var rating = $(this).index();
             putVoteInDB(rating);
-			$(".vote").text("Du röstade " + rating + ", tack för din röst!");
+			$(".vote").html("Du röstade " + "<b>" + rating + "</b>" + ", tack för din röst!");
 	});
 	
 	$(".voteIndicator span").hover(function(){ //sets hovered star to grey together with previous ones
-			$(this).css("cursor", "pointer"); //change cursor to pointer on mouseover
-			$(this).prevAll().css("color", "grey");
-			$(this).css("color", "grey");
-			$(this).animate({fontSize: "2.5em"}, 200);
+			$(this).css({
+				cursor: "pointer"
+			}); //change cursor to pointer on mouseover
+			$(this).prevAll().css({
+				color: "grey"
+			});
+			$(this).css({
+				color: "grey"
+			});
+			$(this).animate({
+				fontSize: "2.5em"
+			}, 200);
 		}, function(){ //change back to default value whem mouse is removed
-			$(this).prevAll().css("color", "black");
-			$(this).css("color", "black");
+			$(this).prevAll().css({
+				color: "black"
+			});
+			$(this).css({
+				color: "black"
+			});
 			$(this).animate({fontSize: "2em"}, 200);
 	});
 		
@@ -37,17 +65,3 @@ $(document).ready(function(){
 $(document).ajaxStart(function(){
      $("#loading").html("<img src='../image/loading.gif'>");
 });
-
-//get rating from API
-function updateVotingResult () {
-    $.ajax({
-        method: "GET",
-        url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=ff5ec25ea3f6b140&recipe=pannkakor",
-        success: function(data) {
-            var rating = data.rating.toFixed(1); //round rating to 1 decimal
-			$("#loading").html("");
-            $(".voteResult strong").text(rating);
-            $(".voteResult em").text(data.votes);
-        },
-    });
-}
